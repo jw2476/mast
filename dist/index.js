ff5dc5c74b7f72d8b00d7499921706093e694448
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2021-08-15 16:04:39 +0100

    Added conversion for javelindata_craftingnames.loc.xml

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,23 @@
-console.log("Hello World");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const fxp = require("fast-xml-parser");
+const DATA_DIR = __dirname + "/../data/";
+function craftingNames() {
+    const raw = fs_1.readFileSync(DATA_DIR + "javelindata_craftingnames.loc.xml", { encoding: "utf-8" });
+    const data = fxp.parse(raw, {
+        ignoreAttributes: false,
+        attributeNamePrefix: "",
+        textNodeName: "value"
+    }).resources.string;
+    let out = {};
+    for (const { key, value } of data) {
+        out[key.split("_CraftName")[0]] = value.split("\\n").join(" ");
+    }
+    fs_1.writeFileSync(DATA_DIR + "javelindata_craftingnames.json", JSON.stringify(out, null, "\t"), {
+        flag: fs_1.existsSync(DATA_DIR + "javelindata_craftingnames.json") ? "w" : "wx"
+    });
+}
+console.log("javelindata_craftingnames.loc.xml -> javelindata_craftingnames.json");
+craftingNames();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World")
+import {readFileSync, writeFileSync, existsSync} from "fs";
+import * as fxp from "fast-xml-parser"
+import Dict = NodeJS.Dict;
+
+const DATA_DIR = __dirname + "/../data/"
+
+function craftingNames() {
+    const raw = readFileSync(DATA_DIR + "javelindata_craftingnames.loc.xml", {encoding: "utf-8"})
+    const data = fxp.parse(raw, {
+        ignoreAttributes: false,
+        attributeNamePrefix: "",
+        textNodeName: "value"
+    }).resources.string
+    let out: Dict<string> = {}
+
+    for (const {key, value} of data) {
+        out[key.split("_CraftName")[0]] = value.split("\\n").join(" ")
+    }
+
+    writeFileSync(DATA_DIR + "javelindata_craftingnames.json", JSON.stringify(out, null, "\t"), {
+        flag: existsSync(DATA_DIR + "javelindata_craftingnames.json") ? "w" : "wx"
+    })
+}
+
+console.log("javelindata_craftingnames.loc.xml -> javelindata_craftingnames.json")
+craftingNames()
