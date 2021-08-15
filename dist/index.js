da8bdef0d17ac8b352b784798d6b94aef8e97905
Jack Whitehorn <jackwh.whitehorn@gmail.com>
2021-08-15 17:00:07 +0100

    Filter javelindata_craftingnames.loc.xml by valid keysd

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const fxp = require("fast-xml-parser");
+const crafting = require("./data/javelindata_crafting.json");
 const DATA_DIR = __dirname + "/../data/";
 function craftingNames() {
     const raw = fs_1.readFileSync(DATA_DIR + "javelindata_craftingnames.loc.xml", { encoding: "utf-8" });
@@ -11,8 +12,14 @@ function craftingNames() {
         textNodeName: "value"
     }).resources.string;
     let out = {};
-    for (const { key, value } of data) {
-        out[key.split("_CraftName")[0]] = value.split("\\n").join(" ");
+    const valid_keys = crafting.map(recipe => recipe.RecipeID);
+    console.log(valid_keys);
+    for (let { key, value } of data) {
+        key = key.split("_CraftName")[0];
+        if (valid_keys.includes(key)) {
+            out[key] = value.split("\\n").join(" ");
+            console.log(key);
+        }
     }
     fs_1.writeFileSync(DATA_DIR + "javelindata_craftingnames.json", JSON.stringify(out, null, "\t"), {
         flag: fs_1.existsSync(DATA_DIR + "javelindata_craftingnames.json") ? "w" : "wx"
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {readFileSync, writeFileSync, existsSync} from "fs";
 import * as fxp from "fast-xml-parser"
 import Dict = NodeJS.Dict;
+import * as crafting from "./data/javelindata_crafting.json"
 
 const DATA_DIR = __dirname + "/../data/"
 
@@ -13,8 +14,14 @@ function craftingNames() {
     }).resources.string
     let out: Dict<string> = {}
 
-    for (const {key, value} of data) {
-        out[key.split("_CraftName")[0]] = value.split("\\n").join(" ")
+    const valid_keys = crafting.map(recipe => recipe.RecipeID)
+    console.log(valid_keys)
+
+    for (let {key, value} of data) {
+        key = key.split("_CraftName")[0]
+        if (valid_keys.includes(key)) {
+            out[key] = value.split("\\n").join(" ")
+        }
     }
 
     writeFileSync(DATA_DIR + "javelindata_craftingnames.json", JSON.stringify(out, null, "\t"), {
